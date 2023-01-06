d106f351feb637d603129b03ef53efa5b1a2e820
Sreeja Prakash <104756330+Sre-eja-09@users.noreply.github.com>
2023-01-07 01:41:32 +0530

    Add files via upload

---
+++ middleofLL.cpp
@@ -0,0 +1,9 @@
+int middleLL(){
+    Node* slow = head;
+    Node* fast = head;
+    while(fast->next != NULL && fast->next->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;   
+    }
+    return slow->data;
+}
